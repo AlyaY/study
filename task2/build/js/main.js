81607f0c7b664a6794f3faac7f3390935128cda4
AlyaY <alya.yurevich@gmail.com>
2018-07-06 16:15:47 +0300

    Start articles block

---
+++ task2/build/js/main.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+    console.log('Load');
+
+    var gridElems = document.querySelectorAll('js-grid');
+}
--- task2/gulpfile.js
+++ task2/gulpfile.js
@@ -1,24 +1,35 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 
+var buildDerectory = 'build/';
+
 gulp.task('style', function () {
     gulp.src('assets/styles/main.styl')
     .pipe(stylus())
-    .pipe(gulp.dest('build/css'));
+    .pipe(gulp.dest(buildDerectory + 'css'));
 });
 
 gulp.task('copy:index', function() {
     gulp.src('assets/index.html')
-    .pipe(gulp.dest('build'));
+    .pipe(gulp.dest(buildDerectory));
 });
 
 gulp.task('copy:images', function() {
     gulp.src('assets/images/*')
-    .pipe(gulp.dest('build/images'));
+    .pipe(gulp.dest(buildDerectory + 'images'));
+});
+
+gulp.task('copy:js', function() {
+    gulp.src('assets/js/*')
+    .pipe(gulp.dest(buildDerectory + 'js'));
 });
 
 gulp.task('watch:style', function () {
     gulp.watch('assets/styles/**/*.styl', ['style']);
+});
+
+gulp.task('watch:js', function () {
+    gulp.watch('assets/js/*.js', ['copy:js']);
 });
 
 gulp.task('watch:index', function () {
@@ -28,4 +39,4 @@ gulp.task('watch:images', function () {
     gulp.watch('assets/images/*', ['copy:images']);
 });
 
-gulp.task('default', ['watch:style', 'watch:index', 'watch:images']);
+gulp.task('default', ['watch:style', 'watch:index', 'watch:images', 'watch:js']);
