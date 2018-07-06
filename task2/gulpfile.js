be7886ccaa2c89456eb793c49e737fdd6df20a7f
AlyaY <alya.yurevich@gmail.com>
2018-07-06 11:34:08 +0300

    Add style for header

--- task2/gulpfile.js
+++ task2/gulpfile.js
@@ -1,8 +1,31 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 
-gulp.task('default', function() {
-    return gulp.src('assets/styles/main.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('build/css'));
+gulp.task('style', function () {
+    gulp.src('assets/styles/main.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('build/css'));
 });
+
+gulp.task('copy:index', function() {
+    gulp.src('assets/index.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('copy:images', function() {
+    gulp.src('assets/images/*')
+    .pipe(gulp.dest('build/images'));
+});
+
+gulp.task('watch:style', function () {
+    gulp.watch('assets/styles/**/*.styl', ['style']);
+});
+
+gulp.task('watch:index', function () {
+    gulp.watch('assets/index.html', ['copy:index']);
+});
+gulp.task('watch:images', function () {
+    gulp.watch('assets/images/*', ['copy:images']);
+});
+
+gulp.task('default', ['watch:style', 'watch:index', 'watch:images']);
