8b4e47d47eb089bfbb94c85bcf6fe0bac6d2054f
AlyaY <alya.yurevich@gmail.com>
2018-07-10 13:31:28 +0300

    Make all articales same height, remove js for grid

--- task2/assets/js/main.js
+++ task2/assets/js/main.js
@@ -1,15 +1,14 @@
 window.onload = function() {
-    var $gridElems = $('.js-gridElement'),
-        $grid = $('.js-gridWrapper'),
-        $slider = $('.js-slider'),
+
+    var $slider = $('.js-slider'),
         $menuButton = $('.js-menuButton');
 
     $slider.length && initSlider($slider);
-    $gridElems.length && $grid.length && initGrid($gridElems, $grid);
     $menuButton.length && menuToggle($menuButton);
 }
 
 function initSlider($slider) {
+    
     $slider.slick({
         infinite: true,
         speed: 500,
@@ -18,33 +17,6 @@ function initSlider($slider) {
         prevArrow: '<button class="intro-slider__arrow _prev"></button>',
         nextArrow: '<button class="intro-slider__arrow _next"></button>'
     });
-}
-
-function initGrid($elems, $grid) {
-    var settings = {
-        rowHeight: parseInt($grid.css('grid-auto-rows')),
-        rowGap: parseInt($grid.css('grid-row-gap')),
-        columnGap: parseInt($grid.css('grid-column-gap')),
-    };
-
-    updateGrid($elems, settings);
-
-    $(window).on('resize', function() {
-        updateGrid($elems, settings);
-    });
-}
-
-function updateGrid($elems, settings) {
-    $elems.each(function(_, elem) {
-        resizeGridItem(elem, settings)
-    });
-}
-
-function resizeGridItem(elem, settings) {
-    rowSpan = Math.ceil(($(elem).outerHeight() + settings.columnGap + settings.rowGap) / (settings.rowHeight+settings.rowGap)); 
-    // <-- (element height) / (grid row height )
-    
-    $(elem).css('grid-row-end', 'span ' + rowSpan);
 }
 
 function menuToggle($menuButton) {
