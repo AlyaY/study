8f8ebfc96880917e2164c99ffbd48b3bb1059119
AlyaY <alya.yurevich@gmail.com>
2018-07-27 14:20:24 +0300

    Change browserRouter to hashRouter for correct gh-page behavior

--- task4/src/modules/Main/views/Main/index.js
+++ task4/src/modules/Main/views/Main/index.js
@@ -12,12 +12,11 @@ const Main = ({ classes }) => {
   return (
     <div className={classes.root}>
       <Switch>
-        <Route exact path='/'/>
+        <Route exact path='/' />
         <Route path='/counters' component={Counter} />
         <Route path='/about' component={About} />
         <Route path='/404' component={NotFound} />
         <Redirect from='*' to='/404' />
-        {/* <Route path='/will-not-match' component={Counter} /> */}
       </Switch>
     </div>
   )
