0f6fd03ffecda32f280919b1747a246be0248398
AlyaY <alya.yurevich@gmail.com>
2018-07-27 13:36:03 +0300

    Add not found page

--- task4/src/modules/Main/views/Main/index.js
+++ task4/src/modules/Main/views/Main/index.js
@@ -5,14 +5,18 @@ import { withStyles } from '@material-ui/core/styles'
 
 import Counter from '../../../Counter'
 import About from '../../../About'
-import { Switch, Route } from 'react-router-dom'
+import NotFound from '../../../NotFound'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 const Main = ({ classes }) => {
   return (
     <div className={classes.root}>
       <Switch>
+        <Route exact path='/'/>
         <Route path='/counters' component={Counter} />
         <Route path='/about' component={About} />
+        <Route path='/404' component={NotFound} />
+        <Redirect from='*' to='/404' />
         {/* <Route path='/will-not-match' component={Counter} /> */}
       </Switch>
     </div>
