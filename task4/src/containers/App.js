b433a3618e07c25472b60de63a39e4106c6e20aa
AlyaY <alya.yurevich@gmail.com>
2018-08-03 12:02:06 +0300

    Remove creation store in seperate file

--- task4/src/containers/App.js
+++ task4/src/containers/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
 
 import Router from '../views/Router';
-import rootReducer from '../reducers';
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(logger)
-);
+import store from '../store';
 
 class App extends Component {
   render () {
