5edb739538afd99ee6d7abff4711e6feb7a9a0a6
AlyaY <alya.yurevich@gmail.com>
2018-08-13 13:53:34 +0300

    Add logger

--- task5/server/app.js
+++ task5/server/app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const rfs = require('rotating-file-stream');
 
 const api = require('./routers/api');
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
+
+const logDirectory = path.join(__dirname, 'log');
+const accessLogStream = rfs('access.log', {
+  interval: '1d',
+  path: logDirectory
+});
+
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+
 
 app
   .use(bodyParser.json())
+  .use(morgan('combined', {stream: accessLogStream}))
   .use('/api', api);
   
 app.listen(PORT, () => console.log(`This work on ${PORT} port`));
